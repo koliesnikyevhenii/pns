fb33c850e7dced98fb5709837bdde4ad47384cf8
koliesnikyevhenii <yevhenii.ko@ideo-digital.com>
2024-10-17 12:04:39 +0300

    Add skeleton for pns menu

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -1,60 +1,65 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
-import AppMenuItem from './AppMenuItem.vue';
+import AppMenuItem from "./AppMenuItem.vue";
 
 const model = ref([
-    {
-        label: 'Home',
-        items: [{ label: 'Applications', icon: 'pi pi-fw pi-home', to: '/' }]
-    },
-    {
-        label: 'Hierarchy',
+  {
+    label: "Home",
+    items: [{ label: "Applications", icon: "pi pi-fw pi-home", to: "/" }],
+  },
+  {
+    label: "Hierarchy",
+    items: [
+      {
+        label: "Messages",
+        icon: "pi pi-fw pi-bookmark",
         items: [
-            {
-                label: 'Messages',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [
-                    {
-                        label: 'Send Message',
-                        icon: 'pi pi-fw pi-bookmark'
-                    },
-                    {
-                        label: 'Message by Alias',
-                        icon: 'pi pi-fw pi-bookmark'
-                    }
-                ]
-            },
-            {
-                label: 'Devices',
-                icon: 'pi pi-fw pi-bookmark'
-            },
-            {
-                label: 'Actions',
-                icon: 'pi pi-fw pi-bookmark'
-            },
-            {
-                label: 'Reports',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [
-                    {
-                        label: 'Messages by tags',
-                        icon: 'pi pi-fw pi-bookmark'
-                    }
-                ]
-            }
-        ]
-    }
+          {
+            label: "Send Message",
+            icon: "pi pi-fw pi-bookmark",
+            to: "/message",
+          },
+          {
+            label: "Message by Alias",
+            icon: "pi pi-fw pi-bookmark",
+            to: "/messagebyalias",
+          },
+        ],
+      },
+      {
+        label: "Devices",
+        icon: "pi pi-fw pi-bookmark",
+        to: "/devices",
+      },
+      {
+        label: "Actions",
+        icon: "pi pi-fw pi-bookmark",
+        to: "/actions",
+      },
+      {
+        label: "Reports",
+        icon: "pi pi-fw pi-bookmark",
+        items: [
+          {
+            label: "Messages by tags",
+            icon: "pi pi-fw pi-bookmark",
+            to: "/reports",
+          },
+        ],
+      },
+    ],
+  },
 ]);
 </script>
 
 <template>
-    <ul class="layout-menu">
-        <template v-for="(item, i) in model" :key="item">
-            <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
-            <li v-if="item.separator" class="menu-separator"></li>
-        </template>
-    </ul>
+  <ul class="layout-menu">
+    <template v-for="(item, i) in model" :key="item">
+      <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
+      <li v-if="item.separator" class="menu-separator"></li>
+    </template>
+  </ul>
 </template>
 
 <style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -14,109 +14,37 @@ const router = createRouter({
                     component: () => import('@/views/Dashboard.vue')
                 },
                 {
-                    path: '/uikit/formlayout',
-                    name: 'formlayout',
-                    component: () => import('@/views/uikit/FormLayout.vue')
-                },
-                {
-                    path: '/uikit/input',
-                    name: 'input',
-                    component: () => import('@/views/uikit/InputDoc.vue')
-                },
-                {
-                    path: '/uikit/button',
-                    name: 'button',
-                    component: () => import('@/views/uikit/ButtonDoc.vue')
-                },
-                {
-                    path: '/uikit/table',
-                    name: 'table',
-                    component: () => import('@/views/uikit/TableDoc.vue')
-                },
-                {
-                    path: '/uikit/list',
-                    name: 'list',
-                    component: () => import('@/views/uikit/ListDoc.vue')
-                },
-                {
-                    path: '/uikit/tree',
-                    name: 'tree',
-                    component: () => import('@/views/uikit/TreeDoc.vue')
-                },
-                {
-                    path: '/uikit/panel',
-                    name: 'panel',
-                    component: () => import('@/views/uikit/PanelsDoc.vue')
-                },
-
-                {
-                    path: '/uikit/overlay',
-                    name: 'overlay',
-                    component: () => import('@/views/uikit/OverlayDoc.vue')
-                },
-                {
-                    path: '/uikit/media',
-                    name: 'media',
-                    component: () => import('@/views/uikit/MediaDoc.vue')
-                },
-                {
-                    path: '/uikit/message',
+                    path: '/message',
                     name: 'message',
-                    component: () => import('@/views/uikit/MessagesDoc.vue')
+                    component: () => import('@/views/pages/SendMessage.vue')
                 },
                 {
-                    path: '/uikit/file',
-                    name: 'file',
-                    component: () => import('@/views/uikit/FileDoc.vue')
+                    path: '/messagebyalias',
+                    name: 'messagebyalias',
+                    component: () => import('@/views/pages/MessageAlias.vue')
                 },
                 {
-                    path: '/uikit/menu',
-                    name: 'menu',
-                    component: () => import('@/views/uikit/MenuDoc.vue')
+                    path: '/devices',
+                    name: 'devices',
+                    component: () => import('@/views/pages/Devices.vue')
                 },
                 {
-                    path: '/uikit/charts',
-                    name: 'charts',
-                    component: () => import('@/views/uikit/ChartDoc.vue')
+                    path: '/actions',
+                    name: 'actions',
+                    component: () => import('@/views/pages/Actions.vue')
                 },
                 {
-                    path: '/uikit/misc',
-                    name: 'misc',
-                    component: () => import('@/views/uikit/MiscDoc.vue')
-                },
-                {
-                    path: '/uikit/timeline',
-                    name: 'timeline',
-                    component: () => import('@/views/uikit/TimelineDoc.vue')
-                },
-                {
-                    path: '/pages/empty',
-                    name: 'empty',
-                    component: () => import('@/views/pages/Empty.vue')
-                },
-                {
-                    path: '/pages/crud',
-                    name: 'crud',
-                    component: () => import('@/views/pages/Crud.vue')
-                },
-                {
-                    path: '/documentation',
-                    name: 'documentation',
-                    component: () => import('@/views/pages/Documentation.vue')
+                    path: '/reports',
+                    name: 'reports',
+                    component: () => import('@/views/pages/Reports.vue')
                 }
             ]
-        },
-        {
-            path: '/landing',
-            name: 'landing',
-            component: () => import('@/views/pages/Landing.vue')
         },
         {
             path: '/pages/notfound',
             name: 'notfound',
             component: () => import('@/views/pages/NotFound.vue')
         },
-
         {
             path: '/auth/login',
             name: 'login',

90280609bc0ab5f57d3fcc92dbbe87b22c512efc
koliesnikyevhenii <yevhenii.ko@ideo-digital.com>
2024-10-17 11:25:36 +0300

    Add initial template commit

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+    <div class="layout-footer">
+        PNS by
+        <a href="https://www.ideo-digital.com/" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Ideo Digital</a>
+    </div>
+</template>

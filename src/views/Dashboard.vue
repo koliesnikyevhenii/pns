20f20151a8793e09364b4be116ae19f4bd551f96
koliesnikyevhenii <yevhenii.ko@ideo-digital.com>
2024-10-18 11:47:41 +0300

    add line chart

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,123 +1,107 @@
 <script setup>
 import { useLayout } from "@/layout/composables/layout";
-import { ProductService } from "@/service/ProductService";
 import { onMounted, ref, watch } from "vue";
 
 const { getPrimary, getSurface, isDarkTheme } = useLayout();
 
-const products = ref(null);
-const chartData = ref(null);
-const chartOptions = ref(null);
+const lineData = ref(null);
+const lineOptions = ref(null);
 
-const items = ref([
-  { label: "Add New", icon: "pi pi-fw pi-plus" },
-  { label: "Remove", icon: "pi pi-fw pi-trash" },
-]);
-
-onMounted(() => {
-  ProductService.getProductsSmall().then((data) => (products.value = data));
-  chartData.value = setChartData();
-  chartOptions.value = setChartOptions();
-});
-
-function setChartData() {
+function setColorOptions() {
   const documentStyle = getComputedStyle(document.documentElement);
-
-  return {
-    labels: ["Q1", "Q2", "Q3", "Q4"],
+  const textColor = documentStyle.getPropertyValue("--text-color");
+  const textColorSecondary = documentStyle.getPropertyValue("--text-color-secondary");
+  const surfaceBorder = documentStyle.getPropertyValue("--surface-border");
+  lineData.value = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
       {
-        type: "bar",
-        label: "Subscriptions",
-        backgroundColor: documentStyle.getPropertyValue("--p-primary-400"),
-        data: [4000, 10000, 15000, 4000],
-        barThickness: 32,
-      },
-      {
-        type: "bar",
-        label: "Advertising",
-        backgroundColor: documentStyle.getPropertyValue("--p-primary-300"),
-        data: [2100, 8400, 2400, 7500],
-        barThickness: 32,
-      },
-      {
-        type: "bar",
-        label: "Affiliate",
-        backgroundColor: documentStyle.getPropertyValue("--p-primary-200"),
-        data: [4100, 5200, 3400, 7400],
-        borderRadius: {
-          topLeft: 8,
-          topRight: 8,
-        },
-        borderSkipped: true,
-        barThickness: 32,
+        label: "PNS",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        backgroundColor: documentStyle.getPropertyValue("--p-primary-500"),
+        borderColor: documentStyle.getPropertyValue("--p-primary-500"),
+        tension: 0.4,
       },
     ],
   };
-}
 
-function setChartOptions() {
-  const documentStyle = getComputedStyle(document.documentElement);
-  const borderColor = documentStyle.getPropertyValue("--surface-border");
-  const textMutedColor = documentStyle.getPropertyValue("--text-color-secondary");
-
-  return {
-    maintainAspectRatio: false,
-    aspectRatio: 0.8,
+  lineOptions.value = {
+    plugins: {
+      legend: {
+        labels: {
+          fontColor: textColor,
+        },
+      },
+    },
     scales: {
       x: {
-        stacked: true,
         ticks: {
-          color: textMutedColor,
+          color: textColorSecondary,
         },
         grid: {
-          color: "transparent",
-          borderColor: "transparent",
+          color: surfaceBorder,
+          drawBorder: false,
         },
       },
       y: {
-        stacked: true,
         ticks: {
-          color: textMutedColor,
+          color: textColorSecondary,
         },
         grid: {
-          color: borderColor,
-          borderColor: "transparent",
-          drawTicks: false,
+          color: surfaceBorder,
+          drawBorder: false,
         },
       },
     },
   };
 }
 
-const formatCurrency = (value) => {
-  return value.toLocaleString("en-US", { style: "currency", currency: "USD" });
-};
-
-watch([getPrimary, getSurface, isDarkTheme], () => {
-  chartData.value = setChartData();
-  chartOptions.value = setChartOptions();
+onMounted(() => {
+  setColorOptions();
 });
+
+watch(
+  [getPrimary, getSurface, isDarkTheme],
+  () => {
+    setColorOptions();
+  },
+  { immediate: true }
+);
 </script>
 
 <template>
   <div class="grid grid-cols-12 gap-8">
     <div class="col-span-12 xl:col-span-6">
       <div class="card">
-        <div class="font-semibold text-xl mb-4">Revenue Stream</div>
-        <Chart type="bar" :data="chartData" :options="chartOptions" class="h-80" />
+        <Button
+          icon="pi pi-file-edit"
+          outlined
+          rounded
+          class="mr-2"
+          @click="editDevice(data)"
+        />
+        <Button
+          icon="pi pi-trash"
+          outlined
+          rounded
+          severity="danger"
+          @click="confirmDeleteDevice(data)"
+        />
+        <div class="font-semibold text-xl mb-4">App Name 1</div>
+        <Chart type="line" :data="lineData" :options="lineOptions" class="h-80" />
       </div>
     </div>
     <div class="col-span-12 xl:col-span-6">
       <div class="card">
-        <div class="font-semibold text-xl mb-4">Revenue Stream</div>
-        <Chart type="bar" :data="chartData" :options="chartOptions" class="h-80" />
+        <div class="font-semibold text-xl mb-4">App Name 2</div>
+        <Chart type="line" :data="lineData" :options="lineOptions" class="h-80" />
       </div>
     </div>
     <div class="col-span-12 xl:col-span-6">
       <div class="card">
-        <div class="font-semibold text-xl mb-4">Revenue Stream</div>
-        <Chart type="bar" :data="chartData" :options="chartOptions" class="h-80" />
+        <div class="font-semibold text-xl mb-4">App Name 3</div>
+        <Chart type="line" :data="lineData" :options="lineOptions" class="h-80" />
       </div>
     </div>
     <div class="col-span-12 xl:col-span-6">
@@ -127,3 +111,9 @@ watch([getPrimary, getSurface, isDarkTheme], () => {
     </div>
   </div>
 </template>
+
+<style scoped>
+.card {
+  padding: 4rem;
+}
+</style>

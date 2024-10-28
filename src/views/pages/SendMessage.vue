1898c77578b0e6f91a447f5e302458759f33af7d
koliesnikyevhenii <yevhenii.ko@ideo-digital.com>
2024-10-28 18:48:23 +0200

    add controls to send message form

--- src/views/pages/SendMessage.vue
+++ src/views/pages/SendMessage.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { PayloadService } from "@/service/PayloadService";
 import { onBeforeMount, ref, computed } from "vue";
-import { useForm, Form } from "vee-validate";
+import { Form } from "vee-validate";
 
 const actionItems = ref(null);
 const selectedAction = ref(null);
+const scheduleRadio = ref("1");
+const sendMode = ref("1");
+const tags = ref(null);
 const action = computed(() => {
   return `{
     "action": "${selectedAction.value?.action ?? ""}"
@@ -55,20 +58,72 @@ onBeforeMount(() => {
           </Fieldset>
           <Fieldset legend="Schedule" :toggleable="false">
             <div class="flex flex-wrap">
-              <label for="payload">Payload:</label>
-              <InputText id="payload" />
+              <div class="flex items-center">
+                <RadioButton
+                  v-model="scheduleRadio"
+                  inputId="scheduleRadio1"
+                  name="scheduleRadio"
+                  value="1"
+                />
+                <label for="scheduleRadio1" class="ml-1 pe-5"
+                  >Begin send immediately</label
+                >
+              </div>
+              <div class="flex items-center">
+                <RadioButton
+                  v-model="scheduleRadio"
+                  inputId="scheduleRadio2"
+                  name="scheduleRadio"
+                  value="2"
+                />
+                <label for="scheduleRadio2" class="ml-1 pe-5"
+                  >Begin send at a particular time</label
+                >
+              </div>
             </div>
           </Fieldset>
           <Fieldset legend="Audience" :toggleable="false">
             <div class="flex flex-wrap">
+              <div class="flex items-center">
+                <RadioButton
+                  v-model="sendMode"
+                  inputId="sendMode1"
+                  name="sendMode"
+                  value="1"
+                />
+                <label for="scheduleRadio1" class="ml-1 pe-5">Send to Everyone</label>
+              </div>
+              <div class="flex items-center">
+                <RadioButton
+                  v-model="sendMode"
+                  inputId="sendMode2"
+                  name="sendMode"
+                  value="2"
+                />
+                <label for="scheduleRadio2" class="ml-1 pe-5">Send by Tag(s)</label>
+              </div>
+              <div class="flex items-center">
+                <RadioButton
+                  v-model="sendMode"
+                  inputId="sendMode3"
+                  name="sendMode"
+                  value="3"
+                />
+                <label for="scheduleRadio2" class="ml-1 pe-5">Send to Alias(es)</label>
+              </div>
+            </div>
+          </Fieldset>
+          <Fieldset legend="Send by Api" :toggleable="false" v-if="sendMode != 1">
+            <div class="flex flex-wrap" v-if="sendMode == 2">
               <label for="payload">Payload:</label>
               <InputText id="payload" />
             </div>
-          </Fieldset>
-          <Fieldset legend="Send by Api" :toggleable="false">
-            <div class="flex flex-wrap">
-              <label for="payload">Payload:</label>
-              <InputText id="payload" />
+            <div class="flex flex-wrap" v-else>
+              <PickList v-model="tags" dataKey="id" breakpoint="1400px">
+                <template #option="{ option }">
+                  {{ option.name }}
+                </template>
+              </PickList>
             </div>
           </Fieldset>
         </div>
